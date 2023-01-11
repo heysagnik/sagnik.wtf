f7e0dfd308db44f102ec5d682fe86fd49a90fd66
Sagnik Sahoo <sahoosagnik1@gmail.com>
2023-01-11 23:04:43 +0530

    🖍️ coloring

--- website-config.js
+++ website-config.js
@@ -4,25 +4,28 @@ export const works = [
     description: "A hub for all your links",
     logo: "/images/logo-linkees.png",
     link: "https://github.com/heysagnik/linkees",
-    background: "hsla(221, 100%, 98%, 1)",
+    background: "hsla(360, 100%, 98%, 1)",
   },
   {
     name: "screeREC",
     description: "A simple screen recorder",
     logo: "/images/logo-screenrec.png",
     link: "https://screen-rec.vercel.app",
+    background: "hsla(221, 100%, 98%, 1)",
   },
   {
     name: "TweetScoom",
     description: "Turn your tweets to a image",
     logo: "/images/logo-tweetscoom.png",
     link: "http://tweetsc-com.vercel.app/",
+    background: "hsla(55, 100%, 98%, 1)",
   },
   {
     name: "Open-Source Projects",
     description: "Check out my GitHub profile",
     logo: "/images/open-source.png",
     link: "https://github.com/heysagnik/",
+    background: "hsla(130, 100%, 98%, 1)",
   },
   
 ]
