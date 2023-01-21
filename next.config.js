d369dc461182448c936557a3494bb1793ea70a79
Sagnik Sahoo <sahoosagnik1@gmail.com>
2023-01-21 21:27:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images : {
+    domains : ['https://raw.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
