fc3e8112e779c392372a2c2d5c20c481fe4fa477
Sagnik Sahoo <sahoosagnik1@gmail.com>
2023-02-11 01:28:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images : {
-    domains : ['raw.githubusercontent.com', 'em-content.zobj.net','cdn.dribbble.com'/users/112330/screenshots/16392696/media/2]
+    domains : ['raw.githubusercontent.com', 'em-content.zobj.net','cdn.dribbble.com'/users]
   }
 }
 
